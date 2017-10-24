2fb6e026f66e27fc4325f3dc6a9533d35ae552a3
Yassine Tioual <yassine.tioual@gmail.com>
2017-10-24 18:10:11 +0200

    Fetch info of youtube videos in 'url_checker'

--- src/plugins/url_checker.py
+++ src/plugins/url_checker.py
@@ -6,6 +6,33 @@ import event
 import re
 import requests
 from bs4 import BeautifulSoup
+import pafy
+
+import traceback
+
+# Color codes
+colors = {
+        'red' : '\033[91m',
+        'green': '\033[92m',
+        'blue': '\033[94m',
+        }
+
+def colorize(string, color):
+    global colors
+    if not color in colors:
+        return string
+    else:
+        return colors[color] + str(string) + '\033[0m'
+
+def process_youtube(url):
+    video = pafy.new(url)
+    likes = colorize(video.likes, 'green')
+    dislikes = colorize(video.dislikes, 'red')
+    duration = colorize(video.duration, 'blue')
+    title = video.title.encode('utf-8')
+
+    return title + " | " + likes + ":" + dislikes + " | " + duration
+
 
 class Plugin:
 
@@ -29,16 +56,21 @@ class Plugin:
 
         for url in urls:
             try:
+                print url
                 res = requests.get(url, timeout=3, allow_redirects=True)
                 if 'text/html' in res.headers['content-type']:
-                    soup = BeautifulSoup(res.text, 'html.parser')
-                    message = str(soup.title.text.encode('utf-8'))
+                    if res.headers['Server'] == 'YouTube Frontend Proxy':
+                        message = process_youtube(url)
+                    else:
+                        soup = BeautifulSoup(res.text, 'html.parser')
+                        message = str(soup.title.text.encode('utf-8'))
                 else:
                     message = res.headers['content-type']
 
-                message = ''.join(c for c in message if ' ' <= c <= '~')
+                message = ''.join(c for c in message if c not in ('\n', '\b', '\r', '\a', '\f', '\v', '\t'))
                 self.client.priv_msg(recipient, message)
             except:
+                print traceback.format_exc()
                 pass
 
     def help(self, target):
